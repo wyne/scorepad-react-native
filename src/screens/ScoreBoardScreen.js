a427a50f5a695ab06370e7f5858a2e4de9e86899
Justin Wyne <jwyne@atlassian.com>
2021-07-28 18:56:29 -0700

    expanded color palette

--- src/screens/ScoreBoardScreen.js
+++ src/screens/ScoreBoardScreen.js
@@ -5,14 +5,8 @@ import Rounds from '../components/Rounds';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function ScoreBoardScreen({ navigation }) {
-    // https://coolors.co/7d9cd4-de8383-a4d4a7-c188d1-8787d4-a1b8e3-d67187
-    // const palette = ["7d9cd4", "de8383", "a4d4a7", "c188d1", "8787d4", "a1b8e3", "d67187"]
-    const palette = ["01497c", "c25858", "f5c800", "275436"]
-    const fontPalette = ["FFFFFF", "FFFFFF", "000000", "FFFFFF"]
-
-
-    // https://coolors.co/f4f1de-e07a5f-8f5d5d-3d405b-5f797b-81b29a-babf95-f2cc8f
-    // const palette = ["e07a5f", "8f5d5d", "3d405b", "5f797b", "81b29a", "babf95", "f2cc8f"]
+    const palette = ["01497c", "c25858", "f5c800", "275436", "dc902c", "62516a", "755647", "925561"]
+    const fontPalette = ["FFFFFF", "FFFFFF", "000000", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF"]
 
     const players = useSelector(state => state.currentGame.players);
 
