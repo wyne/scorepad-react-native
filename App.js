66ace613aea858cfb4112ce3f78d7ce6142b6a28
Justin Wyne <justinwyne@gmail.com>
2023-03-10 18:13:59 -0800

    Drawer Nav

--- App.js
+++ App.js
@@ -5,6 +5,15 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
+import {
+    createDrawerNavigator,
+    DrawerContentScrollView,
+    DrawerItemList,
+    DrawerItem,
+    useDrawerProgress,
+} from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 import GameScreen from "./src/screens/GameScreen";
 import SettingsScreen from "./src/screens/SettingsScreen";
@@ -52,14 +61,73 @@ const navigator = createStackNavigator(
     },
 );
 
-let Navigation = createAppContainer(navigator);
+function CustomDrawerContent(props) {
+    return (
+        <DrawerContentScrollView {...props}>
+            <View>
+                <DrawerItem label="ScorePad with Rounds v1.2.0" onPress={() => alert('Hi!')} />
+                <DrawerItemList {...props} />
+            </View>
+        </DrawerContentScrollView>
+    );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default class App extends React.Component {
     render() {
         return (
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
-                    <Navigation theme="dark" />
+                    <NavigationContainer>
+                        <Drawer.Navigator
+                            initialRouteName="Home"
+                            drawerContent={props => <CustomDrawerContent {...props} />
+                            } >
+                            <Drawer.Screen name="Home" component={ListScreen}
+                                options={{
+                                    title: 'Game List',
+                                    headerTitle: 'ScorePad with Rounds',
+                                    drawerIcon: ({ tintColor }) => (
+                                        <Icon name="home"
+                                            type="font-awesome"
+                                            tintColor={tintColor}
+                                            size={24} />
+                                    ),
+                                }}
+                            />
+                            <Drawer.Screen name="Game" component={GameScreen}
+                                header={RoundTitle}
+                                options={{
+                                    header: (navigation) => {
+                                        return <RoundTitle />;
+                                    },
+                                    drawerIcon: ({ tintColor }) => (
+                                        <Icon name="gamepad"
+                                            type="font-awesome"
+                                            tintColor={tintColor}
+                                            size={24} />
+                                    ),
+                                }}
+                            />
+                            <Drawer.Screen name="Settings" component={SettingsScreen}
+                                options={{
+                                    headerBackgroundContainerStyle: { backgroundColor: '#000' },
+                                    backgroundColor: '#000',
+                                    sceneContainerStyle: {
+                                        backgroundColor: '#000',
+                                    },
+                                    drawerIcon: ({ tintColor }) => (
+                                        <Icon name="cog"
+                                            type="font-awesome"
+                                            tintColor={tintColor}
+                                            size={24} />
+                                    ),
+                                }}
+                                headerStyle={{ backgroundColor: '#000' }}
+                            />
+                        </Drawer.Navigator>
+                    </NavigationContainer>
                 </PersistGate>
             </Provider>
         );
