39436b4aea86c086ac0225ae5f96bba1aa7d632f
Justin Wyne <justinwyne@gmail.com>
2023-08-01 23:59:26 -0700

    Convert SettingsHeader to TypeScript

--- src/components/Headers/SettingsHeader.tsx
+++ src/components/Headers/SettingsHeader.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { ParamListBase } from '@react-navigation/native';
 
-import { useSelector } from 'react-redux';
+import { useAppSelector } from '../../../redux/hooks';
 import { selectGameById } from '../../../redux/GamesSlice';
 import CheckButton from '../Buttons/CheckButton';
 import MenuButton from '../Buttons/MenuButton';
 import CustomHeader from './CustomHeader';
 
-function SettingsHeader({ navigation }) {
-    const currentGame = useSelector(state => selectGameById(state, state.settings.currentGameId));
+interface Props {
+    navigation: NativeStackNavigationProp<ParamListBase, string, undefined>;
+}
+
+const SettingsHeader: React.FunctionComponent<Props> = ({ navigation }) => {
+    const currentGame = useAppSelector(state => selectGameById(state, state.settings.currentGameId));
 
     return (
         <CustomHeader navigation={navigation}
             headerLeft={<MenuButton navigation={navigation} />}
-            headerCenter={<Text style={styles.title}>{currentGame.title}</Text>}
+            headerCenter={<Text style={styles.title}>{currentGame?.title}</Text>}
             headerRight={<CheckButton navigation={navigation} />}
         />
     );
