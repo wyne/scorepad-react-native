2c771441d6153b0f402190c3d491ed687132c3a7
Justin Wyne <justinwyne@gmail.com>
2023-08-09 23:46:48 -0700

    v2.1.12-53

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ const IS_DEV = process.env.APP_VARIANT === 'development';
 export default {
   name: IS_DEV ? 'ScorePad with Rounds (dev)' : 'ScorePad with Rounds',
   slug: 'scorepad',
-  version: "2.1.11",
+  version: "2.1.12",
   orientation: "default",
   icon: IS_DEV ? './assets/icon-dev.png' : './assets/icon.png',
   assetBundlePatterns: [
@@ -14,7 +14,7 @@ export default {
     bundleIdentifier: IS_DEV ? 'com.wyne.scorepad.dev' : 'com.wyne.scorepad',
     supportsTablet: true,
     requireFullScreen: false,
-    buildNumber: "52",
+    buildNumber: "53",
     infoPlist: {
       RCTAsyncStorageExcludeFromBackup: false
     },
@@ -28,7 +28,7 @@ export default {
     },
     package: IS_DEV ? 'com.wyne.scorepad.dev' : 'com.wyne.scorepad',
     permissions: [],
-    versionCode: 52,
+    versionCode: 53,
     googleServicesFile: "./google-services.json",
   },
   userInterfaceStyle: "dark",
