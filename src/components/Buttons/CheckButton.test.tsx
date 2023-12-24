d4d18f268be3c254a9e6f402bee0d32bb626faf4
Justin Wyne <1986068+wyne@users.noreply.github.com>
2023-12-24 02:10:47 -0500

    update tests

--- src/components/Buttons/CheckButton.test.tsx
+++ src/components/Buttons/CheckButton.test.tsx
@@ -8,8 +8,17 @@ import CheckButton from './CheckButton';
 describe('CheckButton', () => {
     const navigation = useNavigationMock();
 
+    it('should navigate to Game screen when pressed', async () => {
+        const { getByRole } = render(<CheckButton navigation={navigation} route={{ key: "", name: "Settings", params: { reason: "new_game" } }} />);
+        const button = getByRole('button');
+        fireEvent.press(button);
+        await waitFor(() => {
+            expect(navigation.navigate).toHaveBeenCalledWith("Game");
+        });
+    });
+
     it('should navigate back a screen when pressed', async () => {
-        const { getByRole } = render(<CheckButton navigation={navigation} />);
+        const { getByRole } = render(<CheckButton navigation={navigation} route={{ key: "", name: "Settings", params: { reason: "" } }} />);
         const button = getByRole('button');
         fireEvent.press(button);
         await waitFor(() => {
