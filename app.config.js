c6d95f7912a540216826545758f00a02f2adee74
Justin Wyne <1986068+wyne@users.noreply.github.com>
2023-12-25 01:53:56 -0500

    v2.4.6-67

--- app.config.js
+++ app.config.js
@@ -42,7 +42,7 @@ switch (variant) {
 export default {
   name: name,
   slug: 'scorepad',
-  version: "2.4.5",
+  version: "2.4.6",
   orientation: "default",
   icon: icon,
   assetBundlePatterns: [
@@ -53,7 +53,7 @@ export default {
     bundleIdentifier: packageName,
     supportsTablet: true,
     requireFullScreen: false,
-    buildNumber: "66",
+    buildNumber: "67",
     infoPlist: {
       RCTAsyncStorageExcludeFromBackup: false
     },
@@ -67,7 +67,7 @@ export default {
     },
     package: packageName,
     permissions: [],
-    versionCode: 66,
+    versionCode: 67,
     googleServicesFile: "./google-services.json",
   },
   userInterfaceStyle: "dark",
