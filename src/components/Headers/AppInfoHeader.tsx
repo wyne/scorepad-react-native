c5314c62aa425ad6ff32ef3fbcdcd31ed56eec7f
Justin Wyne <1986068+wyne@users.noreply.github.com>
2024-07-21 00:11:25 -0700

    Fix android presentation issues

--- src/components/Headers/AppInfoHeader.tsx
+++ src/components/Headers/AppInfoHeader.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import { ParamListBase } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { systemBlue } from '../../constants';
 import HeaderButton from '../Buttons/HeaderButton';
@@ -14,22 +15,35 @@ interface Props {
 
 const AppInfoHeader: React.FunctionComponent<Props> = ({ navigation }: Props) => {
 
+    const Wrapper = Platform.OS == 'ios' ? View : SafeAreaView;
+
     return (
-        <View style={{
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-            flexDirection: 'row',
-            backgroundColor: '#F2F2F7',
-        }}>
-            <HeaderButton
-                accessibilityLabel='Save Game'
-                onPress={async () => {
-                    navigation.goBack();
-                }}>
-                <Text style={{ color: systemBlue, fontSize: 20 }}>Done</Text>
-            </HeaderButton>
-        </View>
+        <Wrapper style={Platform.OS == 'ios' ? undefined : styles.headerContainer} >
+            <View style={{
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                flexDirection: 'row',
+                backgroundColor: '#F2F2F7',
+            }}>
+                <HeaderButton
+                    accessibilityLabel='Save Game'
+                    onPress={async () => {
+                        navigation.goBack();
+                    }}>
+                    <Text style={{ color: systemBlue, fontSize: 20 }}>Done</Text>
+                </HeaderButton>
+            </View>
+        </Wrapper>
     );
 };
 
 export default AppInfoHeader;
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        backgroundColor: '#F2F2F7',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        textAlign: 'center',
+    },
+});
