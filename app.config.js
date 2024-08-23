0cb4f6ba3ac481e9c4b39016548e70c55d9d8a80
Justin Wyne <1986068+wyne@users.noreply.github.com>
2024-08-22 23:04:01 -0700

    v2.5.8-84

--- app.config.js
+++ app.config.js
@@ -59,7 +59,7 @@ switch (variant) {
 export default {
     name: name,
     slug: 'scorepad',
-    version: '2.5.7',
+    version: '2.5.8',
     orientation: 'default',
     icon: icon,
     assetBundlePatterns: ['assets/*'],
@@ -68,7 +68,7 @@ export default {
         bundleIdentifier: packageName,
         supportsTablet: true,
         requireFullScreen: false,
-        buildNumber: '83',
+        buildNumber: '84',
         infoPlist: {
             RCTAsyncStorageExcludeFromBackup: false,
         },
@@ -83,7 +83,7 @@ export default {
         package: packageName,
         permissions: [],
         blockedPermissions: ['android.permission.ACTIVITY_RECOGNITION'],
-        versionCode: 83,
+        versionCode: 84,
         googleServicesFile: './google-services.json',
     },
     userInterfaceStyle: 'dark',
