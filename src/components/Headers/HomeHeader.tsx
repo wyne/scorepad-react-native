44750a29944dbfa862753406192458ed015c5364
Justin Wyne <1986068+wyne@users.noreply.github.com>
2026-05-16 18:36:00 -0400

    Fix new game button

--- src/components/Headers/HomeHeader.tsx
+++ src/components/Headers/HomeHeader.tsx
@@ -1,37 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import { ParamListBase } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { Text, StyleSheet, View } from 'react-native';
+import { ParamListBase } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Text, StyleSheet, View } from "react-native";
 
-import AppInfoButton from '../Buttons/AppInfoButton';
-import NewGameButton from '../Buttons/NewGameButton';
+import AppInfoButton from "../Buttons/AppInfoButton";
+import NewGameButton from "../Buttons/NewGameButton";
 
-import CustomHeader from './CustomHeader';
+import CustomHeader from "./CustomHeader";
 
 interface Props {
-    navigation: NativeStackNavigationProp<ParamListBase, string, undefined>;
+  navigation: NativeStackNavigationProp<ParamListBase, string, undefined>;
 }
 
 const HomeHeader: React.FunctionComponent<Props> = ({ navigation }) => {
-    return (
-        <View style={{ backgroundColor: 'white' }}>
-            <CustomHeader navigation={navigation}
-                headerLeft={<AppInfoButton navigation={navigation} />}
-                headerCenter={<Text style={styles.title} allowFontScaling={false}>ScorePad</Text>}
-                headerRight={<NewGameButton navigation={navigation} />}
-                animated={true}
-            />
-        </View>
-    );
+  return (
+    <View style={{ backgroundColor: "white" }}>
+      <CustomHeader
+        navigation={navigation}
+        headerLeft={<AppInfoButton navigation={navigation} />}
+        headerCenter={
+          <Text style={styles.title} allowFontScaling={false}>
+            ScorePad
+          </Text>
+        }
+        headerRight={<NewGameButton navigation={navigation} />}
+        animated={true}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    title: {
-        color: 'white',
-        fontSize: 20,
-        fontVariant: ['tabular-nums'],
-    },
+  title: {
+    color: "white",
+    fontSize: 20,
+    fontVariant: ["tabular-nums"],
+  },
 });
 
 export default HomeHeader;
