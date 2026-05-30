d3f9e0b2a7a1f25d1a690df9bb694f04ae802bcf
Justin Wyne <1986068+wyne@users.noreply.github.com>
2026-05-30 14:50:48 -0400

    Change devmenu to 10 taps

--- src/components/AppInfo/RotatingIcon.tsx
+++ src/components/AppInfo/RotatingIcon.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import * as Haptics from 'expo-haptics';
 import { Image } from 'expo-image';
@@ -15,8 +15,13 @@ import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 import { toggleDevMenuEnabled } from '../../../redux/SettingsSlice';
 import { logEvent } from '../../Analytics';
 
+const TAP_COUNT_REQUIRED = 10;
+const TAP_RESET_TIMEOUT = 2000;
+
 const RotatingIcon: React.FunctionComponent = () => {
     const dispatch = useAppDispatch();
+    const tapCountRef = useRef(0);
+    const resetTimerRef = useRef<ReturnType<typeof setTimeout>>();
 
     const installId = useAppSelector(state => state.settings.installId);
 
@@ -29,15 +34,30 @@ const RotatingIcon: React.FunctionComponent = () => {
         };
     });
 
-    return <TouchableOpacity testID="app-icon"
-        onLongPress={() => {
+    const handleTap = () => {
+        tapCountRef.current += 1;
+
+        if (resetTimerRef.current) {
+            clearTimeout(resetTimerRef.current);
+        }
+
+        if (tapCountRef.current >= TAP_COUNT_REQUIRED) {
+            tapCountRef.current = 0;
             Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
             dispatch(toggleDevMenuEnabled());
             logEvent('dev_menu', {
                 installId,
             });
             rotation.value = withTiming(rotation.value + 360, { duration: 1000, easing: Easing.elastic(1) });
-        }}>
+        } else {
+            resetTimerRef.current = setTimeout(() => {
+                tapCountRef.current = 0;
+            }, TAP_RESET_TIMEOUT);
+        }
+    };
+
+    return <TouchableOpacity testID="app-icon"
+        onPress={handleTap}>
         <Animated.View style={[animatedStyles]}>
             <Image source={icon}
                 contentFit='contain'
