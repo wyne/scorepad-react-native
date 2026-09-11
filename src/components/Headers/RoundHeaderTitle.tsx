ec4c086d5a6ab3d1621a837192aae59d4fa28bde
Justin Wyne <1986068+wyne@users.noreply.github.com>
2026-09-10 21:37:42 -0400

    fix: tighten Android round controls

--- src/components/Headers/RoundHeaderTitle.tsx
+++ src/components/Headers/RoundHeaderTitle.tsx
@@ -202,6 +202,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderRadius: Platform.OS === 'android' ? 24 : 22,
         height: Platform.OS === 'android' ? 48 : 44,
+        width: Platform.OS === 'android' ? 192 : undefined,
     },
     fallbackContainer: {
         overflow: 'hidden',
